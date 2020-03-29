b6235008787673844bd927b8c64f95bfbe3baff2
chanmaoooo <chanmaoooo@gmail.com>
2020-03-29 23:11:38 +0900

    変更のたびに必要？

--- js/main.js
+++ js/main.js
@@ -1,25 +1,12 @@
 'use strict';
 {
   const btn = document.getElementById('btn');
-  // const result = btn.textContent;
 
   btn.addEventListener('click', ()=> {
     const results = ['大吉', '吉', '凶', '末吉'];
     const n = Math.floor(Math.random()*results.length);
 
     btn.textContent = results[n];
-
-    // switch(n) {
-    //   case 0:
-    //     btn.textContent = '大吉';
-    //     break;
-    //   case 1:
-    //     btn.textContent = '吉';
-    //     break;
-    //   case 2:
-    //     btn.textContent = '凶';
-    //     break;
-    // }
   });
 
 }
